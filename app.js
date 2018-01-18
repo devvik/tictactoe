7224329c126ca1dad65c1d1956996d1d4fe5da27
Vik <vikram.me@gmail.com>
2018-01-17 17:03:17 -0800

    Logic to end game past 9th turn

--- app.js
+++ app.js
@@ -2,23 +2,24 @@ $(document).ready(function(){
     let player = 0; // 0 = 'X', 1 = 'O'
     let turns = 0; 
 
-    //alternate clicks between X & O
     $('.box').click(function() {
-        console.log("Turn= "+turns);
-        console.log("Player= "+player);
-        //if (!$(this).val()) { 
-          if (Boolean(player)) {
+        if (turns>8) {
+            alert("Game Over. Refresh page to restart.");
+        } else {
+            console.log("Turn= "+turns);
+            console.log("Player= "+player);
+            if (Boolean(player)) {
             $(this).val("O");
-          } else {
+            } else {
             $(this).val("X");
-          }
-          turns++;
-          player = turns % 2;
-          console.log("Next Turn= "+turns);
-          console.log("Next Player= "+player);
-          evaluateBoard();
-          console.log("===============================");
-       //}
+            }
+            turns++;
+            player = turns % 2;
+            console.log("Next Turn= "+turns);
+            console.log("Next Player= "+player);
+            evaluateBoard();
+            console.log("===============================");
+        }
     });
 
     function evaluateBoard() {
@@ -27,35 +28,48 @@ $(document).ready(function(){
         //top row same value
         if (($('#r0c0').val() === $('#r0c1').val()) && ($('#r0c0').val() === $('#r0c2').val())) {
             console.log("Winner: " + $('#r0c0').val());
+            $('#winner').html("Winner: " + $('#r0c0').val());
+            gameOver();
         }
         //middle row same value
         if (($('#r1c0').val() === $('#r1c1').val()) && ($('#r1c0').val() === $('#r1c2').val())) {
             console.log("Winner: " + $('#r1c0').val());
+            gameOver();
         }
         //bottom row same value
         if (($('#r2c0').val() === $('#r2c1').val()) && ($('#r2c0').val() === $('#r2c2').val())) {
             console.log("Winner: " + $('#r2c0').val());
+            gameOver();
         }
         //left most column same value
         if (($('#r0c0').val() === $('#r1c0').val()) && ($('#r0c0').val() === $('#r2c0').val())) {
             console.log("Winner: " + $('#r0c0').val());
+            gameOver();
         }
         //middle column same value
         if (($('#r0c1').val() === $('#r1c1').val()) && ($('#r0c1').val() === $('#r2c1').val())) {
             console.log("Winner: " + $('#r0c1').val());
+            gameOver();
         }
         //right most column same value
         if (($('#r0c2').val() === $('#r1c2').val()) && ($('#r0c2').val() === $('#r2c2').val())) {
             console.log("Winner: " + $('#r0c2').val());
+            gameOver();
         }
         //diagonal from top left corner has same value
         if (($('#r0c0').val() === $('#r1c1').val()) && ($('#r0c0').val() === $('#r2c2').val())) {
             console.log("Winner: " + $('#r0c0').val());
+            gameOver();
         }
         //diagonal from bottom left corner has same value
         if (($('#r2c0').val() === $('#r1c1').val()) && ($('#r2c0').val() === $('#r0c2').val())) {
             console.log("Winner: " + $('#r2c0').val());
+            gameOver();
         }
+    }
+
+    function gameOver() {
+        //turns=10; //to stop the game
     }
 
 });
